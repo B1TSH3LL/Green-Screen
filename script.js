1cd5bcd8c51d8c08c20bc69ad63fa45323288995
CyberBreach <shrivastavapiyush781@gmail.com>
2021-09-25 20:44:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+var fgImage = null;
+var bgImage = null;
+var fgCanvas;
+var bgCanvas;
+
+function loadForegroundImage() {
+  var file = document.getElementById("fgfile");
+  fgImage = new SimpleImage(file);
+  fgCanvas = document.getElementById("fgcan");
+  fgImage.drawTo(fgCanvas);
+}
+
+function loadBackgroundImage() {
+  var file = document.getElementById("bgfile");
+  bgImage = new SimpleImage(file);
+  bgCanvas = document.getElementById("bgcan");
+  bgImage.drawTo(bgCanvas);
+}
+
+function createComposite() {
+  var output = new SimpleImage(fgImage.getWidth(),fgImage.getHeight());
+  var greenThreshold = 240;
+  for (var pixel of fgImage.values()) {
+    var x = pixel.getX();
+    var y = pixel.getY();
+    if (pixel.getGreen() > greenThreshold) {
+      var bgPixel = bgImage.getPixel(x,y);
+      output.setPixel(x,y,bgPixel);
+    }
+    else {
+      output.setPixel(x,y,pixel);
+    }
+  }
+  return output;
+}
+
+function doGreenScreen() {
+  if (fgImage == null  || ! fgImage.complete()) {
+    alert("Foreground image not loaded");
+  }
+  if (bgImage == null || ! bgImage.complete()) {
+    alert("Background image not loaded");
+  }
+  clearCanvas();
+  var finalImage = createComposite();
+  finalImage.drawTo(fgCanvas);
+}
+
+function clearCanvas() {
+  doClear(fgCanvas);
+  doClear(bgCanvas);
+}
+
+function doClear(canvas) {
+  var context = canvas.getContext("2d");
+  context.clearRect(0,0,canvas.width,canvas.height);
+}
